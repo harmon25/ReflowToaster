39d0b617e54cfa7229dccf61cb578019498d34b6
harmoN <nomraharmon@gmail.com>
2017-07-15 18:06:31 -0400

    add boot.py

---
+++ boot.py
@@ -0,0 +1,23 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import gc
+import webrepl
+webrepl.start()
+gc.collect()
+
+SSID = "*****"
+PSK = "*****"
+
+def do_connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(SSID, PSK)
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+
+do_connect()
